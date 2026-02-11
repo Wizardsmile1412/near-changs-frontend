61a98ef1eb581f10b5b9807c71a14bf56a2e0843
Pawarit Sripayom <psripayom.s@wisdomvast.com>
2026-02-11 13:29:36 +0700

    chore(.env): add .env.example and apply env varible to code base for i18n

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -1,4 +1,12 @@
-export const locales = ["en", "th"] as const;
+const supportedLocales = process.env.NEXT_PUBLIC_SUPPORTED_LOCALES?.split(
+    ",",
+) ?? ["en", "th"];
+
+export const locales = supportedLocales as unknown as readonly [
+    string,
+    ...string[],
+];
 export type Locale = (typeof locales)[number];
 
-export const defaultLocale: Locale = "th";
+export const defaultLocale: Locale =
+    process.env.NEXT_PUBLIC_DEFAULT_LOCALE ?? "en";
